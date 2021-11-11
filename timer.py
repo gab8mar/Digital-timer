c50f4dfb107224710f69f59978819eb3b5801f2e
Gab8mar <92567231+gab8mar@users.noreply.github.com>
2021-11-11 16:38:55 -0500

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,21 @@
+import time
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.geometry("400x200")
+ventana.resizable(False,False)
+ventana.title("TIMER")
+ventana.config(background='#C0C0C0')
+time_label = tk.Label(text=time.strftime('%H:%M:%S'), font=('Arial', 54), fg='black', pady=10, padx=10)
+time_label.place(x=50, y=40)
+print(time_label['text'])
+
+def update_reloj():
+	ahora = time.strftime('%H:%M:%S')
+	if time_label['text'] != ahora: 
+		  time_label['text'] = ahora
+
+	ventana.after(1000, update_reloj)
+	
+update_reloj()		  
+ventana.mainloop()
